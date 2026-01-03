661dde9362a42390ddee40aa2be3deae8447fa25
Spongeeli <jimrsnider@gmail.com>
2026-01-03 11:12:30 -0600

    Shaderless Default Triangle

--- 3D Renderer/main.cpp
+++ 3D Renderer/main.cpp
@@ -12,6 +12,7 @@ int main() {
     }
 
     // Actually make the window
+    // Found out that Wayland compositor system scaling bugs this out, will have to look into framebuffer
     window = glfwCreateWindow(1280, 720, "Renderer Window", nullptr, nullptr);
 
     // Verify window creation and terminate GLFW if error
@@ -33,9 +34,29 @@ int main() {
         return -1;
     }
 
+    // Current testing code (default triangle)
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    const GLfloat triangleData[9] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f
+    };
+
+    GLuint bufferID;
+    glGenBuffers(1, &bufferID);
+    glBindBuffer(GL_ARRAY_BUFFER, bufferID);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangleData), triangleData, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (const void *) 0);
+    
     // Actual draw/refresh body to fill in later
     while(!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
